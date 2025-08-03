0c2e2c7609e2cd0ec1d0079b6693041e9653baf3
Priscilla Zhu <prischillazhu272@gmail.com>
2025-08-03 01:17:17 -0400

    Initial commit of meal picker backbone

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import random
+
+app = Flask(__name__)
+CORS(app)
+
+meals = ["Pomodoro Pennoni", "番茄青菜鸡蛋面", "番茄肥牛饭", "番茄肥牛面", "白菜豆腐面"]
+
+@app.route("/random-meal")
+def random_meal():
+    return jsonify({"meal": random.choice(meals)})
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [meal, setMeal] = useState("");
+
+  const getMeal = async () => {
+    const res = await fetch("http://127.0.0.1:5000/random-meal");
+    const data = await res.json();
+    setMeal(data.meal);
+  };
+
+  return (
+    <div className="centered">
+      <div id="title">
+        What should I cook today?
+      </div>
+
+      <br />
+
+      <button onClick={getMeal} className="">
+        Pick a Meal
+      </button>
+
+      <br /> <br />
+
+      {meal && <div id = "choice">I should cook: <strong>{meal}</strong></div>}
+    </div>
+  );
+}
+
+export default App;
