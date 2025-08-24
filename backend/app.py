3367a67994389aa01e373e21a75fba2364e9ecab
Priscilla Zhu <priscillazhu272@gmail.com>
2025-08-24 10:46:46 -0400

    Added food items

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 import random
 
 app = Flask(__name__)
 CORS(app)
 
-meals = ["Pomodoro Pennoni", "番茄青菜鸡蛋面", "番茄肥牛饭", "番茄肥牛面", "白菜豆腐面"]
+meals = ["Pomodoro Pennoni", "番茄青菜鸡蛋面", "番茄肥牛饭", "番茄肥牛面", "白菜豆腐面", "Overnight Oats", "Mango Yogurt Bites", "酒酿汤圆", "番茄炒蛋", "Matcha Latte"]
 
 @app.route("/random-meal")
 def random_meal():
